79d0a5b1f22a1acd3a4f54804499848411137464
Navneeth Krishna <mnkb20@gmail.com>
2023-10-05 15:01:59 -0500

    modified create_tables.sql file

--- Subjects/Database 1/Project 1/workspace/create_tables.sql
+++ Subjects/Database 1/Project 1/workspace/create_tables.sql
@@ -1,65 +1,57 @@
--- Create the DEPARTMENT table
-CREATE TABLE DEPARTMENT (
-    Dnumber INT NOT NULL PRIMARY KEY,
-    Dname VARCHAR(15) NOT NULL,
-    Mgr_ssn CHAR(9) NOT NULL UNIQUE,
-    Mgr_start_date DATE
-);
+CREATE TABLE EMPLOYEE
+( Fname           VARCHAR(10)   NOT NULL,
+  Minit           CHAR,
+  Lname           VARCHAR(20)      NOT NULL,
+  Ssn             CHAR(9)          NOT NULL,
+  Bdate           DATE,
+  Address         VARCHAR(30),
+  Sex             CHAR(1),
+  Salary          DECIMAL(6,2),
+  Super_ssn       CHAR(9),
+  Dno             INT               NOT NULL,
+  OverTimeCount INT DEFAULT 0, -- Added 'OverTimeCount' attribute
+PRIMARY KEY   (Ssn));
 
--- Create the EMPLOYEE table with OverTimeCount attribute
-CREATE TABLE EMPLOYEE (
-    Fname VARCHAR(15) NOT NULL,
-    Minit CHAR,
-    Lname VARCHAR(15) NOT NULL,
-    Ssn CHAR(9) NOT NULL PRIMARY KEY,
-    Bdate DATE,
-    Address VARCHAR(30),
-    Sex CHAR(1),
-    Salary DECIMAL(10, 2),
-    Super_ssn CHAR(9),
-    Dno INT NOT NULL,
-    OverTimeCount INT DEFAULT 0, -- Added 'OverTimeCount' attribute
-    FOREIGN KEY (Dno) REFERENCES DEPARTMENT(Dnumber),
-    FOREIGN KEY (Super_ssn) REFERENCES EMPLOYEE(Ssn)
-);
+CREATE TABLE DEPARTMENT
+( Dname           VARCHAR(15)       NOT NULL,
+  Dnumber         INT               NOT NULL,
+  Mgr_ssn         CHAR(9)           NOT NULL,
+  Mgr_start_date  DATE,
+PRIMARY KEY (Dnumber),
+UNIQUE      (Dname),
+FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn) );
 
--- Create the DEPT_LOCATIONS table
-CREATE TABLE DEPT_LOCATIONS (
-    Dnumber INT NOT NULL,
-    Dlocation VARCHAR(15) NOT NULL,
-    PRIMARY KEY (Dnumber, Dlocation),
-    FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber)
-);
+CREATE TABLE DEPT_LOCATIONS
+( Dnumber         INT               NOT NULL,
+  Dlocation       VARCHAR(15)       NOT NULL,
+PRIMARY KEY (Dnumber, Dlocation),
+FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber) );
 
--- Create the PROJECT table
-CREATE TABLE PROJECT (
-    Pname VARCHAR(15) NOT NULL,
-    Pnumber INT NOT NULL PRIMARY KEY,
-    Plocation VARCHAR(15),
-    Dnum INT NOT NULL,
-    FOREIGN KEY (Dnum) REFERENCES DEPARTMENT(Dnumber)
-);
+CREATE TABLE PROJECT
+( Pname           VARCHAR(15)       NOT NULL,
+  Pnumber         INT               NOT NULL,
+  Plocation       VARCHAR(15),
+  Dnum            INT               NOT NULL,
+PRIMARY KEY (Pnumber),
+UNIQUE      (Pname),
+FOREIGN KEY (Dnum) REFERENCES DEPARTMENT(Dnumber) );
 
--- Create the WORKS_ON table
-CREATE TABLE WORKS_ON (
-    Essn CHAR(9) NOT NULL,
-    Pno INT NOT NULL,
-    Hours DECIMAL(3, 1) NOT NULL,
-    PRIMARY KEY (Essn, Pno),
-    FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
-    FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber)
-);
+CREATE TABLE WORKS_ON
+( Essn            CHAR(9)           NOT NULL,
+  Pno             INT               NOT NULL,
+  Hours           DECIMAL(3,1)      NOT NULL,
+PRIMARY KEY (Essn, Pno),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber) );
 
--- Create the DEPENDENT table
-CREATE TABLE DEPENDENT (
-    Essn CHAR(9) NOT NULL,
-    Dependent_name VARCHAR(15) NOT NULL,
-    Sex CHAR(1),
-    Bdate DATE,
-    Relationship VARCHAR(8),
-    PRIMARY KEY (Essn, Dependent_name),
-    FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn)
-);
+CREATE TABLE DEPENDENT
+( Essn            CHAR(9)           NOT NULL,
+  Dependent_name  VARCHAR(15)       NOT NULL,
+  Sex             CHAR,
+  Bdate           DATE,
+  Relationship    VARCHAR(8),
+PRIMARY KEY (Essn, Dependent_name),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn) );
 
 -- Create the trigger to update OverTimeCount
 DELIMITER //
@@ -85,3 +77,14 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+
+ALTER TABLE DEPARTMENT
+ ADD CONSTRAINT Dep_emp FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn);
+
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_emp FOREIGN KEY (Super_ssn) REFERENCES EMPLOYEE(Ssn);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_dno FOREIGN KEY  (Dno) REFERENCES DEPARTMENT(Dnumber);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_super FOREIGN KEY  (Super_ssn) REFERENCES EMPLOYEE(Ssn);
